c40a7090c425310cb228bfee9422bba1520db0b6
jasti <vamseej@gmail.com>
2014-01-19 14:33:30 -0500

    Base Rest Framework

---
+++ src/test/dash/TestSecurityDao.java
@@ -0,0 +1,36 @@
+package dash;
+
+import java.util.Iterator;
+import java.util.List;
+
+import com.finThink.order.Order;
+import com.finThink.security.Security;
+import com.finThink.security.SecurityDAO;
+
+public class TestSecurityDao {
+
+	
+	public static void main(String[] args) {
+		SecurityDAO securityDao = new SecurityDAO();
+		
+		List<Security> securities = securityDao.findAll();
+		
+		for (Iterator iterator = securities.iterator(); iterator.hasNext();) {
+			Security security = (Security) iterator.next();
+			System.out.println("Sec ID : "+security.getSecId() +" Sec Name: " +security.getSecName());
+			
+		}
+		
+		// Find Trades for a security
+		
+	List<Order> orders = securityDao.findOrdersBySecurity(1);
+		
+		for (Iterator iterator = orders.iterator(); iterator.hasNext();) {
+			Order order = (Order) iterator.next();
+			System.out.println(order);
+			
+		}
+		
+		
+	}
+}
